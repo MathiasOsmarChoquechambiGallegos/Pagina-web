80ab390270fe21c807cd6f4b182ef62de54a81e5
Cristhian Rospigliosi <68781691+christtian1e@users.noreply.github.com>
2025-09-27 05:28:02 -0500

    Update Home.jsx

--- Home.jsx
+++ Home.jsx
@@ -1,24 +1,31 @@
-import chicogg from './assets/ggez.jpg'
-import gato from './assets/gatojeje.png'
+import { Link} from 'react-router-dom';
+import '../App.css';
+import arequipa from './arequipa.jpg'
+import turista from './turistas.jpeg'
 
-export default function Home() {
-  return (
-    <main>
-      <div className="hero">
-        <h1>¡Explora el Perú con nosotros!</h1>
-        <p>Anímate a descubrir los lugares más hermosos que el Perú te puede ofrecer y vive una experiencia única en tu travesía.</p>
-        <button className="explore-btn">Explorar</button>
-      </div>
-      
-      <div className="content-section">
-        <h2>Explora Arequipa!</h2>
-        <img src={gato} alt="Imagen turística de Arequipa"/>
-      </div>
-      
-      <div className="content-section">
-        <h2>Comparte tu experiencia!</h2>
-        <img src={chicogg} alt="Representación del usuario escribiendo su opinión"/>
-      </div>
-    </main>
-  )
+export default function ListUser() {
+    
+    return (
+        <main>
+            <>
+                <div className="hero">
+                    <h1>¡Explora el Perú con nosotros!</h1>
+                    <p>Anímate a descubrir los lugares más hermosos que el Perú te puede ofrecer y vive una experiencia única en tu travesía.</p>
+                    <Link to="atraccion/search"><button className="explore-btn">Explorar</button></Link>
+                </div>
+            
+                <div className="content-section">
+                    <h2>Explora Arequipa!</h2>
+                    <img src={arequipa} alt="Imagen turística de Arequipa"/><br></br>
+                </div>
+                
+                <div className="content-section">
+                    <h2>Comparte tu experiencia!</h2>
+                    <img src={turista} alt="Representación del usuario escribiendo su opinión"/>
+                </div>
+
+            </>
+        </main>
+        
+    )
 }
